cb01ac8f4f8926eeaabddf53f0bba114f8c5e05a
Mateusz Koza <mateusz.koza@grinn-global.com>
2025-05-10 15:31:35 +0200

    Add the lraspp student model, Restructure code

---
+++ distillers/semantic_segmentation_distiller.py
@@ -0,0 +1,84 @@
+import torch
+from torch import nn
+import lightning as L
+
+
+class SemanticSegmentationDistiller(L.LightningModule):
+    def __init__(
+        self,
+        teacher,
+        student,
+        kd_weight=0.5,
+        kd_temperature=2.0,
+    ):
+        super().__init__()
+        self.teacher = teacher
+        self.teacher.eval()
+
+        self.student = student
+
+        self.kd_weight = kd_weight
+        self.kd_temperature = kd_temperature
+        self.kd_criterion = nn.KLDivLoss(reduction="batchmean")
+
+    def forward(self, x):
+        return self.student(x)["out"]
+
+    def training_step(self, batch):
+        x = batch["image"]
+        y = batch["mask"]
+
+        y_hat_s = self(x)
+        loss_target = self.teacher.criterion(y_hat_s, y)
+
+        if self.kd_weight == 0:
+            loss = loss_target
+        else:
+            with torch.no_grad():
+                y_hat_t = self.teacher(x).output
+            loss_kd = self.kd_criterion(
+                torch.log_softmax(y_hat_s / self.kd_temperature, dim=1),
+                torch.softmax(y_hat_t / self.kd_temperature, dim=1),
+            ) * (self.kd_temperature**2)
+            self.log("train/loss_target", loss_target, on_epoch=True, on_step=False)
+            self.log("train/loss_kd", loss_kd, on_epoch=True, on_step=False)
+
+            loss = self.kd_weight * loss_kd + (1 - self.kd_weight) * loss_target
+
+        self.log("train/loss", loss, on_epoch=True, on_step=False)
+        self.teacher.train_metrics.update(y_hat_s.argmax(dim=1), y)
+        return loss
+
+    def validation_step(self, batch):
+        x = batch["image"]
+        y = batch["mask"]
+        y_hat_s = self(x)
+        loss = self.teacher.criterion(y_hat_s, y)
+        self.teacher.val_metrics.update(y_hat_s.argmax(dim=1), y)
+        self.log("val/loss", loss, on_epoch=True, on_step=False)
+
+    def test_step(self, batch):
+        x = batch["image"]
+        y = batch["mask"]
+        y_hat_s = self(x)
+        loss = self.teacher.criterion(y_hat_s, y)
+        self.teacher.test_metrics[0].update(y_hat_s.argmax(dim=1), y)
+        self.log("test/loss", loss, on_epoch=True, on_step=False)
+
+    def on_train_epoch_end(self):
+        metrics = self.teacher.train_metrics.compute()
+        self.log_dict(metrics, on_epoch=True, on_step=False)
+        self.teacher.train_metrics.reset()
+
+    def on_validation_epoch_end(self):
+        metrics = self.teacher.val_metrics.compute()
+        self.log_dict(metrics, on_epoch=True, on_step=False)
+        self.teacher.val_metrics.reset()
+
+    def on_test_epoch_end(self):
+        metrics = self.teacher.test_metrics[0].compute()
+        self.log_dict(metrics, on_epoch=True, on_step=False)
+        self.teacher.test_metrics[0].reset()
+
+    def configure_optimizers(self):
+        return torch.optim.AdamW(self.parameters(), lr=0.0001)
---
+++ students/lraspp_mobilenet_v3_large.py
@@ -0,0 +1,33 @@
+from torch import nn
+from torchvision.models.segmentation import (
+    lraspp_mobilenet_v3_large,
+    LRASPP_MobileNet_V3_Large_Weights,
+)
+
+
+class LRASPPMobileNetV3Large(nn.Module):
+    def __init__(self, num_channels, num_classes):
+        super().__init__()
+
+        self.model = lraspp_mobilenet_v3_large(
+            weights=LRASPP_MobileNet_V3_Large_Weights.DEFAULT,
+        )
+        self.model.backbone["0"][0] = nn.Conv2d(
+            num_channels,
+            16,
+            kernel_size=(3, 3),
+            stride=(2, 2),
+            padding=(1, 1),
+            bias=False,
+        )
+
+        self.model.classifier.low_classifier = nn.Conv2d(
+            40, num_classes, kernel_size=(1, 1), stride=(1, 1)
+        )
+
+        self.model.classifier.high_classifier = nn.Conv2d(
+            128, num_classes, kernel_size=(1, 1), stride=(1, 1)
+        )
+
+    def forward(self, x):
+        return self.model(x)
